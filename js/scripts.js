4992a3d76a5cd87819a1541ea382fe13ca78a2d6
Brian Muchunku <brauxer@gmail.com>
2019-10-01 15:51:45 +0300

    Set the home image h as a background image in css, add hide functionality to the toggle icons design, development and product management icons in css and toggle code using jquery

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function){(
+  $("button").click(function(){
+    $("hide1").toggle();
+  });
+  $("button").click(function(){
+    $("hide2").toggle();
+  });
+  $("button").click(function(){
+    $("hide3").toggle();
+  });
+});
+  
